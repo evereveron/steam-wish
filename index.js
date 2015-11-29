cc431b0717202307f47afcfb3e78db56dc9afb2b
Rosheen <rosheenmail@gmail.com>
2015-11-28 21:36:00 -0500

    working on dashboard frontend

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ var mysql = require('mysql');
 var app = express();
 var request = require('request'),
 		cheerio = require('cheerio'),
-		fs = require('fs'),
 		appidList = [];
+var wishlistURL;
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -16,21 +16,6 @@ var connection = mysql.createConnection({
 	password: 'steamgame',
 	port 	: '3306'
 });
-
-/*
-//JQuery Example Use
-var options = {
-	host: 'store.steampowered.com',
-	port: 80,
-	path: '/'
-};
-
-var html = '';
-http.get(options, function(res) {
-	//$("body").append("<div>TEST</div>");
-	console.log($("menuitem").html());
-	});
-*/
 
 app.use(express.static(__dirname + '/public'));
 
@@ -51,18 +36,20 @@ app.get('/index', function (request, response) {
 	 response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
 //wildcard (if nothing matches)
 app.get('*', function (req, res) {
 	res.send('Bad Route: URL does not exist');
 
 });
 
+app.listen(app.get('port'), function() {
+	console.log('Node app is running on port', app.get('port'));
+});
+
+/***************** CUSTOM METHODS BELOW ***********************/
 /*GET RID OF HARD CODING ONCE USER INPUT IS FIGURED OUT*/
-request('http://steamcommunity.com/id/T1War/wishlist/', function(err, resp, body){
+wishlistURL = 'http://steamcommunity.com/id/T1War/wishlist/';
+request(wishlistURL, function(err, resp, body){
 	if(!err && resp.statusCode == 200){
 		var $ = cheerio.load(body);
 		$('div.wishlistRow').each(function(){
@@ -76,46 +63,13 @@ request('http://steamcommunity.com/id/T1War/wishlist/', function(err, resp, body
 	}
 });
 
-/*
-request('http://www.reddit.com', function(err, resp, body){
-	if(!err && resp.statusCode == 200){
-		var $ = cheerio.load(body);
-		$('a.title', '#siteTable').each(function(){
-			var url = $(this).attr('href');
-			if(url.indexOf('i.imgur.com') != -1){
-				urls.push(url);
-			}
-		});
+connection.connect();
 
-		console.log(urls);
-	}
+var query = connection.query('SELECT * FROM SteamGame.tags', function(err, result, fields){
+	if(err) throw err;
+		//console.log('result:', result);
 });
-*/
 
-/*ATTEMPT AT GETTING USER INPUT---FIGURE OUT HOW TO DO LATER*/
-/*
-app.post("/", function(req){
-	console.log("hello");
-	var urlName = req.body.url;
-	request(urlName, function(err, resp, body){
-		if(!err && resp.statusCode == 200){
-			alert(urlName);
-			var $ = cheerio.load(body);
-			$('h4.ellipsis').each(function(){
-				urls.push(url);
-			});
-		}
-		console.log(urls);
-	});
-});
-*/
-
-
-//app.use(express.bodyParser());
-
-/*app.post("/", function(request, response){
-	console.log(request.body.urlName);
-	//console.log(request.body.user.email);
-});*/
+connection.end();
 
 
